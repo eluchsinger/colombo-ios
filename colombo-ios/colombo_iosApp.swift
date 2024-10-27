4db28cf5f8595ca20d366762d88ba0761d7ba6ab
Esteban Luchsinger <eluchsingerm@hotmail.com>
2024-10-27 18:11:34 +0100

    :tada: Initial Commit

---
+++ colombo-ios/colombo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  colombo_iosApp.swift
+//  colombo-ios
+//
+//  Created by Esteban Luchsinger on 27.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct colombo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ colombo-iosTests/colombo_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  colombo_iosTests.swift
+//  colombo-iosTests
+//
+//  Created by Esteban Luchsinger on 27.10.2024.
+//
+
+import Testing
+@testable import colombo_ios
+
+struct colombo_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
